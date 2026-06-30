7fb64056f89cf47bc8434f99da1742aee6029290
Chatdanai2605 <qqalm123@gmail.com>
2026-06-30 15:46:32 +0700

    end

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 print("hello")
-a - ("tr")
-B = ("gh")
-print (a+b)
+print("inforation and network engineering")
+print("Anirach you will be great")
+
+print("I'm Hungry")
+print('He ask me "who got the book"')
+
+print("I'm good and \"I will do wy best\"")
+print('/\\')
+
