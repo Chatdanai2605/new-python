d71633727598b4cd2789377d0bf6f3cbe72eddbe
Chatdanai2605 <qqalm123@gmail.com>
2026-07-07 15:35:44 +0700

    en

---
+++ Lecture-02/EXAMPLE3.py
@@ -0,0 +1,4 @@
+celsius = input("Enter temperature in Celsius: ")
+celsius = float(celsius)
+fahrenheit = (celsius * 9/5) +32
+print("temperture in fahrenheit is:"+ format(fahrenheit, ".2f"))
---
+++ Lecture-02/Example2.py
@@ -0,0 +1,7 @@
+
+
+
+weight = int(input("Enter your weight in kg :"))
+height = float(input("Enter your height in meters: "))
+bmi = weight / (height ** 2 )
+print("you bmi is ",format(bmi ,".2f"))
